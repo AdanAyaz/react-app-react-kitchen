482026836b2a230b1137a693b582d23e9180d9eb
AdanAyaz <91949524+AdanAyaz@users.noreply.github.com>
2022-01-17 19:00:07 +0500

    Update OrderPanel.js

--- src/components/Main/OrderPanel/OrderPanel.js
+++ src/components/Main/OrderPanel/OrderPanel.js
@@ -72,33 +72,3 @@ function OrderPanel(props) {
 
 export default OrderPanel;
 
-//////////////////////////
-
-// const [foodState, setFoodState] = useState([]);
-// // AN API CALL ON A BUTTON CREATED FOR NO FRICKING REASON - Time waste
-
-// const foodOrdering = async () => {
-//   let menuContent = await fetch(
-//     "http://openlibrary.org/search.json?q=harry%20potter&fields=*,availability&limit=1"
-//   );
-//   if (!menuContent.ok) {
-//     return console.log("An error has occurred. Sorry for the inconvenience.");
-//   }
-//   let data = menuContent.json();
-//   data = data.then((data) => {
-//     console.log(data.docs[0]);
-//     setFoodState([data.docs[0], ...foodState]);
-//   });
-
-{
-  /* <button className="request-data-button" onClick={foodOrdering}>
-        Request Data
-      </button>
-      {foodState.map((ele) => {
-        return (
-          <div className="product-ind-display">
-            <p style={{ display: "inline" }}>{ele.title_suggest}</p>
-          </div>
-        );
-      })} */
-}
