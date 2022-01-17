71bbdfb7e18eb70a58ab26c25a851e1a085578f4
AdanAyaz <91949524+AdanAyaz@users.noreply.github.com>
2022-01-17 18:39:28 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Navbar from "./components/Navbar/Navbar";
 import Main from "./components/Main/Main";
 import orderContext from "./store/order-context-";
+import { useState } from "react";
 
 function App() {
+  let [currentOrderData, setCurrentOrderData] = useState([]);
+
+  function dataRecieveHandler00(ele) {
+    setCurrentOrderData(ele);
+  }
+
   return (
     <div>
-      <Navbar></Navbar>
-      <Main></Main>
+      <Navbar onDataCoined={currentOrderData}></Navbar>
+      <Main onOrderDataRecieve={dataRecieveHandler00}></Main>
     </div>
   );
 }
